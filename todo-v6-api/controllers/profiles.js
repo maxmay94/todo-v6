170386ceb7e9df29d3715851e82909829f939a57
maxmay94 <maxmay94@gmail.com>
2022-10-14 12:52:15 -0400

    remove profile photos

--- todo-v6-api/controllers/profiles.js
+++ todo-v6-api/controllers/profiles.js
@@ -1,5 +1,4 @@
 import { Profile } from '../models/profile.js'
-import { v2 as cloudinary } from 'cloudinary'
 
 function index(req, res) {
   Profile.find({})
@@ -7,25 +6,6 @@ function index(req, res) {
   .catch(err => {
     console.log(err)
     res.status(500).json(err)
-  })
-}
-
-function addPhoto(req, res) {
-  const imageFile = req.files.photo.path
-  Profile.findById(req.params.id)
-  .then(profile => {
-    cloudinary.uploader.upload(imageFile, {tags: `${profile.email}`})
-    .then(image => {
-      profile.photo = image.url
-      profile.save()
-      .then(profile => {
-        res.status(201).json(profile.photo)
-      })
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json(err)
-    })
   })
 }
 
--- todo-v6-api/models/profile.js
+++ todo-v6-api/models/profile.js
@@ -1,10 +1,9 @@
 import mongoose from 'mongoose'
-
 const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
+  email: {type:String, required: true, lowercase: true, unique: true},
   name: String,
-  photo: String
 },{
   timestamps: true,
 })
