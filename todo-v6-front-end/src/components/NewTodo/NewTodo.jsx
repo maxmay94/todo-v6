f792c04ae5213029838cb8a9551cdd3e9b573d9f
maxmay94 <maxmay94@gmail.com>
2022-10-17 13:06:04 -0400

    get all and create todo services added

---
+++ todo-v6-front-end/src/components/NewTodo/NewTodo.jsx
@@ -0,0 +1,9 @@
+const NewTodo = ({createTodo, newTodo, setNewTodo}) => {
+  return (
+    <div className="bg-amber-400 w-2/3 mx-auto text-center my-5 p-5 drop-shadow-xl rounded">
+      
+    </div>
+  )
+}
+
+export default NewTodo
---
+++ todo-v6-front-end/src/services/todoService.js
@@ -0,0 +1,34 @@
+import * as tokenService from '../services/tokenService'
+
+const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/todos`
+
+export const getAllTodos = async() => {
+  try{
+    const res = await fetch(BASE_URL)
+    const data = await res.join()
+    return data
+  } catch(err) {
+    throw err
+  }
+}
+
+export const createTodo = async(todo) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'content-type' : 'application/json',
+        'Authorization' : `Bearer ${tokenService.getToken()}`
+      },
+      body: JSON.stringify({
+        title: todo,
+        completed: false
+      })
+    })
+    const data = await res.join()
+    return data
+  } catch(err) {
+    throw err
+  }
+}
+
